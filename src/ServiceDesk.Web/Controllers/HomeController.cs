bd2e8862cf4988bb5f11c38587bd4317c3359282
btchoum <bertrand.tchoumkeu@gmail.com>
2021-04-25 12:53:41 -0500

    add web project with individual user accounts authentication

---
+++ src/ServiceDesk.Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ServiceDesk.Web.Models;
+
+namespace ServiceDesk.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Home - Index Page hit");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
